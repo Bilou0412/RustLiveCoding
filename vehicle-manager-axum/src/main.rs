64649a05fcdb3ff294c99c1168bc10a0435cf01e
Bilel Moudache <bilel.moudache@proton.me>
2025-11-27 14:54:02 +0100

    building two endpoint : POST ,GET for learning Rust axum freamwork

--- vehicle-manager-axum/src/main.rs
+++ vehicle-manager-axum/src/main.rs
@@ -1,54 +1,37 @@
 use axum::{
-    routing::get,
-    routing::post,
-    Json,
-    Router,
+    Json, Router, routing::{get, post}
 };
+use serde::{Deserialize,Serialize};
 
-use serde::Deserialize;
-
-#[derive(Deserialize,Debug)]
-struct CreateVehicle{
+#[derive(Deserialize)]
+struct VehicleData {
     brand: String,
+    years: u32,
     model: String,
-    year: u32,
 }
 
+#[derive(Serialize)]
+struct ResponseMessage {
+    status: String,
+    message: String,
+}
 #[tokio::main]
 async fn main() {
-    // 1. METS UN POINT D'ARRÊT ICI (Ligne A)
-    println!("Configuration du routeur..."); 
-    
-    // On appelle une fonction nommée 'handler_hello' au lieu de le faire inline
+    // build our application with a single route
     let app = Router::new()
-    .route("/", get(handler_hello))
-    .route("/vehicle", post(create_vehicle));
-
-    println!("Le serveur va démarrer sur le port 3000...");
-    
+    .route("/ok", get(hello_world))
+    .route("/vehicle_data", post(vehicle_data));
+  
+    // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    
-    // Le debugger va 'attendre' ici que des requêtes arrivent
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler_hello() -> &'static str {
-    let message = "Hello, World!";   
-
-    // 🔴 METS LE POINT ROUGE ICI (Ligne 24 par exemple)
-    println!("Debug value: {}", message); 
-
-    println!("J'ai reçu une visite !");
-    message 
+async fn hello_world() -> Json<ResponseMessage> {
+    Json(ResponseMessage { status: "success".to_string(), message: "hello world".to_string()})
 }
 
-async fn create_vehicle(Json(payload): Json<CreateVehicle>) -> String {
-    
-    // 🔴 3. METS TON POINT D'ARRÊT (BREAKPOINT) ICI (Ligne 31)
-    println!("Nouvelle voiture reçue !");
-    
-    // Ici, tu pourras inspecter 'payload' dans le debugger
-    let reponse = format!("Voiture créée : {} {} ({})", payload.brand, payload.model, payload.year);
-    
-    reponse
+async fn vehicle_data(Json(payload): Json<VehicleData>) -> Json<ResponseMessage> 
+{
+    Json(ResponseMessage { status: "success".to_string(), message: format!("On a bien save le vehicule: {},{},{}",payload.brand,payload.model,payload.years)  })
 }
